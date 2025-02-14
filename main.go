4cfa73dfcd07363477cc5f08c59867b0a8d3bd73
thachsolum <thach.svh@solu-m.com>
2025-02-14 08:49:06 +0700

    chore(main): remove test code

--- main.go
+++ main.go
@@ -4,21 +4,6 @@ import (
 	_ "github.com/solum-sp/aps-be-common/common/cache"
 	_ "github.com/solum-sp/aps-be-common/common/config"
 	_ "github.com/solum-sp/aps-be-common/common/event"
-	"github.com/solum-sp/aps-be-common/common/logger"
 	_ "github.com/solum-sp/aps-be-common/common/logger"
 	_ "github.com/solum-sp/aps-be-common/common/utils"
 )
-
-func main() {
-	l, err := logger.NewLogger(logger.Config{
-		Service: "test",
-		Level:   logger.DebugLv,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	l.Error("Hello, world!", "key", "value")
-	l.Info("Hello, world!", "key", "value")
-
-}
