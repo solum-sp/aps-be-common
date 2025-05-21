f4a0ae824181e39809d6960746a5a6ae54c3c0c2
Văn Huỳnh <van.svh@sol-m.com>
2025-05-21 10:39:13 +0700

    feat: add http server

---
+++ common/httpserver/httpserver.go
@@ -0,0 +1,193 @@
+package httpserver
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/solum-sp/aps-be-common/common/logger"
+
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+type HttpServerConfig struct {
+	Host             string   `env:"HTTP_HOST" envDefault:"localhost"`
+	Port             int      `env:"HTTP_PORT" envDefault:"8020"`
+	AllowOrigins     []string `env:"HTTP_CORS_ALLOW_ORIGINS" envSeparator:"," envDefault:"*"`
+	AllowMethods     []string `env:"HTTP_CORS_ALLOW_METHODS" envSeparator:"," envDefault:"GET,POST,PUT,DELETE,OPTIONS"`
+	AllowHeaders     []string `env:"HTTP_CORS_ALLOW_HEADERS" envSeparator:"," envDefault:"Origin,Content-Type,Authorization"`
+	AllowCredentials bool     `env:"HTTP_CORS_ALLOW_CREDENTIALS" envDefault:"false"`
+}
+type RouteRegistrar interface {
+	Register(server *HTTPServer, group *gin.RouterGroup)
+}
+type RouteConfig struct {
+	Method      string
+	Path        string
+	Handler     gin.HandlerFunc
+	Description string
+	Params      string
+	Middlewares []gin.HandlerFunc
+}
+
+var methods = struct{
+	GET string
+	POST string
+	PUT string
+	DELETE string
+}{
+	GET: "GET",
+	POST: "POST",
+	PUT: "PUT",
+	DELETE: "DELETE",
+}
+var DefaultConfig = HttpServerConfig{
+	Host: "localhost",
+	Port: 8080,
+}
+
+type HTTPServer struct {
+	config HttpServerConfig
+	logger logger.ILogger
+	router *gin.Engine
+	registrars map[string][]RouteRegistrar
+	globalMiddlewares []gin.HandlerFunc
+}
+
+type Option func(*HTTPServer)
+
+func NewHTTPServer(opts ...Option) *HTTPServer {
+	gin.SetMode(gin.ReleaseMode)
+	cfg := HttpServerConfig{}
+	hs := &HTTPServer{
+		config: cfg,
+		router: gin.Default(),
+	}
+
+	// Apply functional options
+	for _, opt := range opts {
+		opt(hs)
+	}
+
+	if len(hs.globalMiddlewares) > 0 {
+		hs.router.Use(hs.globalMiddlewares...)
+	}
+	// Final setup
+	hs.SetupRouter()
+
+	return hs
+}
+
+func (s *HTTPServer) Initialize() *HTTPServer {
+	s.SetupRouter()
+	return s
+}
+
+func (s *HTTPServer) SetupRouter() {
+	s.ServeSwagger()
+	for version, regs := range s.registrars {
+		var group *gin.RouterGroup
+		if version == "root" {
+			group = s.router.Group("")
+			} else {
+				group = s.router.Group("/" + version) // just /v1, /v2
+			}
+		for _, reg := range regs {
+			reg.Register(s, group)
+		}
+	}
+}
+
+func (s *HTTPServer) ServeSwagger() {
+	s.router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+}
+
+
+func (s *HTTPServer) addRouteV2(group *gin.RouterGroup, cfg RouteConfig) {
+	handler := cfg.Handler
+	if len(cfg.Middlewares) > 0 {
+		handler = chainMiddlewares(cfg.Handler, cfg.Middlewares...)
+	}
+	params := cfg.Params
+	if strings.TrimSpace(params) == "" {
+		params = "no required params"
+	}
+	joinPaths := s.joinPaths(group.BasePath(), cfg.Path)
+	if group == nil {
+		s.router.Handle(cfg.Method, cfg.Path, handler)
+		s.logger.Info("Route initialized", "Method", cfg.Method, "Path", cfg.Path, "Description", cfg.Description, "Params", params)
+	} else {
+		group.Handle(cfg.Method, cfg.Path, handler)
+		s.logger.Info("Route initialized", "Method", cfg.Method, "Path", joinPaths, "Description", cfg.Description, "Params", params)
+	}
+}
+
+func chainMiddlewares(final gin.HandlerFunc, middlewares ...gin.HandlerFunc) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		for _, m := range middlewares {
+			m(ctx)
+			if ctx.IsAborted() {
+				return
+			}
+		}
+		final(ctx)
+	}
+}
+func (s *HTTPServer) Start() error {
+	addr := fmt.Sprintf("%s:%d", s.config.Host, s.config.Port)
+
+	s.logger.Info(fmt.Sprintf("Starting HTTP server on address: %v...", addr))
+	if err := s.router.Run(addr); err != nil {
+		s.logger.Error(fmt.Sprintf("Failed to start HTTP server: %s", err))
+		return err
+	}
+
+	return nil
+}
+
+// === Optional configuration like logger, system config,.... ===
+func WithLogger(logger logger.ILogger) Option {
+	return func(s *HTTPServer) {
+		s.logger = logger
+	}
+}
+
+func WithConfig(c HttpServerConfig) Option {
+	return func(s *HTTPServer) {
+		if c.Host == "" && c.Port == 0 {
+			s.config = DefaultConfig
+		} else {
+			s.config = c
+		}
+
+		// ✅ Ensure AllowOrigins & AllowHeaders are not nil, fallback to defaults
+		if len(s.config.AllowOrigins) == 0 {
+			s.config.AllowOrigins = DefaultConfig.AllowOrigins
+		}
+		if len(s.config.AllowHeaders) == 0 {
+			s.config.AllowHeaders = DefaultConfig.AllowHeaders
+		}
+	}
+}
+
+func WithVersionedRegistrars(regs map[string][]RouteRegistrar) Option {
+	return func(s *HTTPServer) {
+		s.registrars = regs
+	}
+}
+
+func WithGlobalMiddlewares(middlewares ...gin.HandlerFunc) Option {
+	return func(s *HTTPServer) {
+		s.globalMiddlewares = append(s.globalMiddlewares, middlewares...)
+	}
+}
+func (s *HTTPServer) joinPaths(base, path string) string {
+	if base == "" {
+		return path
+	}
+	if path == "" {
+		return base
+	}
+	return strings.TrimRight(base, "/") + "/" + strings.TrimLeft(path, "/")
+}
--- go.mod
+++ go.mod
@@ -21,11 +21,44 @@ require (
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/bytedance/sonic v1.11.6 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/spec v0.20.4 // indirect
+	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.20.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/swaggo/swag v1.8.12 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	golang.org/x/arch v0.8.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/tools v0.23.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
+)
+
+require (
 	aidanwoods.dev/go-result v0.3.1 // indirect
 	github.com/alicebob/gopher-json v0.0.0-20230218143504-906a9b012302 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gin-gonic/gin v1.10.1
 	github.com/golobby/container/v3 v3.3.2
 	github.com/hamba/avro/v2 v2.24.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
@@ -38,11 +71,13 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/swaggo/files v1.0.1
+	github.com/swaggo/gin-swagger v1.6.0
 	github.com/yuin/gopher-lua v1.1.1 // indirect
 	go.opentelemetry.io/otel v1.34.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
